67d4f21fb09332191d6a742d5921925a2d9d4a3c
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-08-05 09:32:08 +0100

    added this

--- KWSDemo/kwssdk/src/main/java/kws/superawesome/tv/kwssdk/KWS.java
+++ KWSDemo/kwssdk/src/main/java/kws/superawesome/tv/kwssdk/KWS.java
@@ -46,7 +46,7 @@ public class KWS implements KWSManagerInterface, PushManagerInterface, CheckMana
     private KWSCheckInterface checkListener;
 
     public String getVersion () {
-        return "android-1.2.1";
+        return "android-1.2.3";
     }
 
     // <Setup> functions
