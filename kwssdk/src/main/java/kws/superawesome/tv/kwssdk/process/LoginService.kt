03a0069ef1f34bfa51c9827c826134d5dfc3d82e
Guilherme Mota <guilherme.mota@superawesome.tv>
2017-12-08 17:42:58 +0000

    Gui test commit. Added Base lib to handle network requests, parsing and caching. Updated gradle to 3.0.1. Added support dependencies to match SDK version 27. Updated MyApplication to extend MultiDexApplication.

---
+++ kwssdk/src/main/java/kws/superawesome/tv/kwssdk/process/LoginService.kt
@@ -0,0 +1,8 @@
+package kws.superawesome.tv.kwssdk.process
+
+/**
+ * Created by guilherme.mota on 08/12/2017.
+ */
+interface LoginService {
+    //TODO implement the new KWService (need to create it, BaseService) as PJService
+}
---
+++ kwssdk/src/main/java/kws/superawesome/tv/kwssdk/providers/LoginProvider.kt
@@ -0,0 +1,7 @@
+package kws.superawesome.tv.kwssdk.providers
+
+/**
+ * Created by guilherme.mota on 08/12/2017.
+ */
+class LoginProvider {
+}
